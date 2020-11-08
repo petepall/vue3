ce0110c9746d38edec248789d99d2a5972b3b197
Peter Pallen <peter@ppallen.be>
2020-11-09 00:06:15 +0100

    Setup navbar + created container layout

--- learn_composition/src/App.vue
+++ learn_composition/src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <router-view />
+  <div class="container">
+    <Navbar />
+    <router-view />
+  </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style lang="scss">
 </style>
---
+++ learn_composition/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar navbar-dark sticky-top bg-dark shadow-lg p-3 mb-5 rounded">
+  <span class="navbar-brand mb-0 h1 ">Navbar</span>
+</nav>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
