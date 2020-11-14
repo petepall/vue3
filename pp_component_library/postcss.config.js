6b4667646e40e77b2118f0a938acb65cd1bec300
Peter Pallen <peter@ppallen.be>
2020-11-14 21:08:20 +0100

    clean

--- pp_component_library/postcss.config.js
+++
@@ -1,6 +0,0 @@
-const autoprefixer = require('autoprefixer')();
-const tailwindcss = require('tailwindcss')('./tailwind.config.js');
-
-module.exports = {
-  plugins: [tailwindcss, autoprefixer],
-};
--- pp_component_library/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-// Setup tailwind css
-import '@/assets/css/tailwind.css';
-
-createApp(App).mount('#app');
