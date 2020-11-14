7acce84bc20461cab3a7b70c6c172f99ee333ff3
Peter Pallen <peter@ppallen.be>
2020-11-14 22:10:37 +0100

    Tailwind base setup

---
+++ pp_component_library/src/App.vue
@@ -0,0 +1,137 @@
+<template>
+  <pp_NavBar />
+  <div class="container mt-20">
+    <h1 class="text-center text-3xl bg-gray-200 text-red-500">
+      This is a header
+    </h1>
+  </div>
+  <p class="text-2xl">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, sit?
+    Aperiam veritatis molestiae eligendi architecto, quae consequatur
+    repellendus dolorem praesentium magnam voluptatem fugiat magni fugit placeat
+    sunt fuga officiis? A dolorem placeat provident quos nostrum atque
+    excepturi? Officiis alias excepturi minima nobis suscipit obcaecati
+    voluptatem, aliquam atque numquam distinctio maiores consequuntur vero odit
+    labore facilis assumenda quae odio dolor molestias, in itaque, reprehenderit
+    earum. Doloremque commodi dolorem molestias culpa consequuntur expedita ad
+    itaque dolore! Autem dolore quibusdam quidem dicta maiores odit quod amet
+    esse magni necessitatibus odio excepturi, nesciunt animi beatae corporis
+    soluta aperiam similique saepe praesentium! Hic, numquam quidem!
+  </p>
+  <p class="text-2xl">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, sit?
+    Aperiam veritatis molestiae eligendi architecto, quae consequatur
+    repellendus dolorem praesentium magnam voluptatem fugiat magni fugit placeat
+    sunt fuga officiis? A dolorem placeat provident quos nostrum atque
+    excepturi? Officiis alias excepturi minima nobis suscipit obcaecati
+    voluptatem, aliquam atque numquam distinctio maiores consequuntur vero odit
+    labore facilis assumenda quae odio dolor molestias, in itaque, reprehenderit
+    earum. Doloremque commodi dolorem molestias culpa consequuntur expedita ad
+    itaque dolore! Autem dolore quibusdam quidem dicta maiores odit quod amet
+    esse magni necessitatibus odio excepturi, nesciunt animi beatae corporis
+    soluta aperiam similique saepe praesentium! Hic, numquam quidem!
+  </p>
+  <p class="text-2xl">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, sit?
+    Aperiam veritatis molestiae eligendi architecto, quae consequatur
+    repellendus dolorem praesentium magnam voluptatem fugiat magni fugit placeat
+    sunt fuga officiis? A dolorem placeat provident quos nostrum atque
+    excepturi? Officiis alias excepturi minima nobis suscipit obcaecati
+    voluptatem, aliquam atque numquam distinctio maiores consequuntur vero odit
+    labore facilis assumenda quae odio dolor molestias, in itaque, reprehenderit
+    earum. Doloremque commodi dolorem molestias culpa consequuntur expedita ad
+    itaque dolore! Autem dolore quibusdam quidem dicta maiores odit quod amet
+    esse magni necessitatibus odio excepturi, nesciunt animi beatae corporis
+    soluta aperiam similique saepe praesentium! Hic, numquam quidem!
+  </p>
+  <p class="text-2xl">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, sit?
+    Aperiam veritatis molestiae eligendi architecto, quae consequatur
+    repellendus dolorem praesentium magnam voluptatem fugiat magni fugit placeat
+    sunt fuga officiis? A dolorem placeat provident quos nostrum atque
+    excepturi? Officiis alias excepturi minima nobis suscipit obcaecati
+    voluptatem, aliquam atque numquam distinctio maiores consequuntur vero odit
+    labore facilis assumenda quae odio dolor molestias, in itaque, reprehenderit
+    earum. Doloremque commodi dolorem molestias culpa consequuntur expedita ad
+    itaque dolore! Autem dolore quibusdam quidem dicta maiores odit quod amet
+    esse magni necessitatibus odio excepturi, nesciunt animi beatae corporis
+    soluta aperiam similique saepe praesentium! Hic, numquam quidem!
+  </p>
+  <p class="text-2xl">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, sit?
+    Aperiam veritatis molestiae eligendi architecto, quae consequatur
+    repellendus dolorem praesentium magnam voluptatem fugiat magni fugit placeat
+    sunt fuga officiis? A dolorem placeat provident quos nostrum atque
+    excepturi? Officiis alias excepturi minima nobis suscipit obcaecati
+    voluptatem, aliquam atque numquam distinctio maiores consequuntur vero odit
+    labore facilis assumenda quae odio dolor molestias, in itaque, reprehenderit
+    earum. Doloremque commodi dolorem molestias culpa consequuntur expedita ad
+    itaque dolore! Autem dolore quibusdam quidem dicta maiores odit quod amet
+    esse magni necessitatibus odio excepturi, nesciunt animi beatae corporis
+    soluta aperiam similique saepe praesentium! Hic, numquam quidem!
+  </p>
+  <p class="text-2xl">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, sit?
+    Aperiam veritatis molestiae eligendi architecto, quae consequatur
+    repellendus dolorem praesentium magnam voluptatem fugiat magni fugit placeat
+    sunt fuga officiis? A dolorem placeat provident quos nostrum atque
+    excepturi? Officiis alias excepturi minima nobis suscipit obcaecati
+    voluptatem, aliquam atque numquam distinctio maiores consequuntur vero odit
+    labore facilis assumenda quae odio dolor molestias, in itaque, reprehenderit
+    earum. Doloremque commodi dolorem molestias culpa consequuntur expedita ad
+    itaque dolore! Autem dolore quibusdam quidem dicta maiores odit quod amet
+    esse magni necessitatibus odio excepturi, nesciunt animi beatae corporis
+    soluta aperiam similique saepe praesentium! Hic, numquam quidem!
+  </p>
+  <p class="text-2xl">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, sit?
+    Aperiam veritatis molestiae eligendi architecto, quae consequatur
+    repellendus dolorem praesentium magnam voluptatem fugiat magni fugit placeat
+    sunt fuga officiis? A dolorem placeat provident quos nostrum atque
+    excepturi? Officiis alias excepturi minima nobis suscipit obcaecati
+    voluptatem, aliquam atque numquam distinctio maiores consequuntur vero odit
+    labore facilis assumenda quae odio dolor molestias, in itaque, reprehenderit
+    earum. Doloremque commodi dolorem molestias culpa consequuntur expedita ad
+    itaque dolore! Autem dolore quibusdam quidem dicta maiores odit quod amet
+    esse magni necessitatibus odio excepturi, nesciunt animi beatae corporis
+    soluta aperiam similique saepe praesentium! Hic, numquam quidem!
+  </p>
+  <p class="text-2xl">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, sit?
+    Aperiam veritatis molestiae eligendi architecto, quae consequatur
+    repellendus dolorem praesentium magnam voluptatem fugiat magni fugit placeat
+    sunt fuga officiis? A dolorem placeat provident quos nostrum atque
+    excepturi? Officiis alias excepturi minima nobis suscipit obcaecati
+    voluptatem, aliquam atque numquam distinctio maiores consequuntur vero odit
+    labore facilis assumenda quae odio dolor molestias, in itaque, reprehenderit
+    earum. Doloremque commodi dolorem molestias culpa consequuntur expedita ad
+    itaque dolore! Autem dolore quibusdam quidem dicta maiores odit quod amet
+    esse magni necessitatibus odio excepturi, nesciunt animi beatae corporis
+    soluta aperiam similique saepe praesentium! Hic, numquam quidem!
+  </p>
+  <p class="text-2xl">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, sit?
+    Aperiam veritatis molestiae eligendi architecto, quae consequatur
+    repellendus dolorem praesentium magnam voluptatem fugiat magni fugit placeat
+    sunt fuga officiis? A dolorem placeat provident quos nostrum atque
+    excepturi? Officiis alias excepturi minima nobis suscipit obcaecati
+    voluptatem, aliquam atque numquam distinctio maiores consequuntur vero odit
+    labore facilis assumenda quae odio dolor molestias, in itaque, reprehenderit
+    earum. Doloremque commodi dolorem molestias culpa consequuntur expedita ad
+    itaque dolore! Autem dolore quibusdam quidem dicta maiores odit quod amet
+    esse magni necessitatibus odio excepturi, nesciunt animi beatae corporis
+    soluta aperiam similique saepe praesentium! Hic, numquam quidem!
+  </p>
+</template>
+
+<script>
+import pp_NavBar from '@/components/pp_NavBar.vue';
+
+export default {
+  components: {
+    pp_NavBar,
+  },
+};
+</script>
+
+<style lang="scss"></style>
